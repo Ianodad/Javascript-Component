4de75b73021b3c130ce52bd2eb503c2305cc5d8e
Ian Odhiambo <you@example.com>
2019-03-22 16:05:48 +0300

    added alert to intaite the application

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
 // get all get all elements in the document
 const documentSelector = document;
 
+loadEventListeners();
+
+function loadEventListeners() {
+	// document ready//
+	document.addEventListener('DOMContentLoaded', startListening);
+
+	// waits for for a click event
+	documentSelector.addEventListener('click', getElement);
+}
+
 // Event Listeners //
-// waits for for a click event
-documentSelector.addEventListener('click', getElement);
 
 // gets event and target together with all the children nodes
 function getElement(event) {
 	const allChildren = event.target.querySelectorAll('*');
 
+	// nodelist into an array
+	const newChild = Array.prototype.slice.call(allChildren);
 
-	// nodelist into an array 
-	var newChild = Array.prototype.slice.call(allChildren);
-
+	const newLoop = [];
+	// loop throught the array
 	for (let i = 0; i <= newChild.length; i++) {
-		console.log(newChild[i]);
+		newLoop.push(newChild[i]);
 	}
 
-	console.log(newChild.length);
-	console.log(newChild.constructor.name);
+	printElements(newLoop);
+	// console.log(newChild.length);
+	// console.log(newChild.constructor.name);
 	// console.log(allChildren[0]);
+}
+// alert(getElement());
+function printElements(elements) {
+	const stringed = [];
+
+	alert(`There are ${elements.length} tags found in the element selected`);
+
+	elements.forEach((element) => {
+		alert(`"${element.tagName.toLowerCase()}"`);
+	});
+	// alert(element.toLowerCase());
+}
+
+function startListening() {
+	alert('Please Click of any part of the DOM to get tags of elements');
 }
